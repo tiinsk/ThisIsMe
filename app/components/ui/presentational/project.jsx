02027132936a4a04b4da4bd2838e605bcf295171
tiinsk <tiina.koskiranta@gmail.com>
2019-06-18 15:06:26 -0700

    Fix project layout

--- app/components/ui/presentational/project.jsx
+++ app/components/ui/presentational/project.jsx
@@ -9,35 +9,33 @@ const Project = ({strings, project}) => {
 
   return(
     <div className="project">
-      <div className="project-wrapper">
-        <div className="project-image" style={{backgroundImage: `url(${image}`}}/>
-        <div className="project-name">
-          <div>{strings.projects[project.key].name}</div>
-          <div className="icons">
-            <IconButton
-              icon="public"
-              link={project.link}
-              type="material-icons"
-            />
-            <IconButton
-              icon="github"
-              link={project.github}
-              type="fa-icons"
-            />
-          </div>
+      <div className="project-image" style={{backgroundImage: `url(${image}`}}/>
+      <div className="project-name">
+        <div>{strings.projects[project.key].name}</div>
+        <div className="icons">
+          <IconButton
+            icon="public"
+            link={project.link}
+            type="material-icons"
+          />
+          <IconButton
+            icon="github"
+            link={project.github}
+            type="fa-icons"
+          />
         </div>
-        <div className="project-description">
-          {strings.projects[project.key].description}
-        </div>
-        <div className="project-skills">
-          {
-            project.skills.map( (skill,i) => {
-              return (
-                <Skill key={i} skill={skill}/>
-              )
-            })
-          }
-        </div>
+      </div>
+      <div className="project-description">
+        {strings.projects[project.key].description}
+      </div>
+      <div className="project-skills">
+        {
+          project.skills.map( (skill,i) => {
+            return (
+              <Skill key={i} skill={skill}/>
+            )
+          })
+        }
       </div>
     </div>
   )
