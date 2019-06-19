17021cc0469314ca14d62cd4f450e925df5f2c1c
tiinsk <tiina.koskiranta@gmail.com>
2019-06-18 17:29:07 -0700

    Small fixes

--- app/data/contacts.js
+++ app/data/contacts.js
@@ -1,7 +1,7 @@
 export default {
   name: "Tiina Koskiranta",
-  phone: "+358 50 348 9445",
+  phone: "(714) 737 1679",
   email: "tiina.koskiranta@gmail.com",
   github: "https://github.com/tiinsk",
-  linkedin: "https://www.linkedin.com/in/tiinakoskiranta"
+  linkedin: "https://linkedin.com/in/tiinakoskiranta"
 }
