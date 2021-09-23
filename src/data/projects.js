cb39e21b76bfd251f881186328246b11c6ee666c
tiinsk <tiina.koskiranta@gmail.com>
2021-09-23 18:46:11 +0300

    add food blog project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,68 @@
 export default [
   {
+    key: 'thisIsMe',
+    github: 'https://github.com/tiinsk/ThisIsMe',
+    link: 'https://tiina.dev',
+    images: [{
+      img: 'img_1.jpg',
+      thumb: 'thumb_1.jpg',
+    }, {
+      img: 'img_2.jpg',
+      thumb: 'thumb_2.jpg',
+    }, {
+      img: 'img_3.jpg',
+      thumb: 'thumb_3.jpg',
+    }],
+    topSkills: [
+      'react',
+      'javascript',
+      'ui_design',
+      'sketch',
+    ],
+    otherSkills: [
+      'node',
+      'css',
+      'sass',
+      'git',
+      'digital_ocean',
+      'nginx'
+    ]
+  },
+  {
+    key: 'foodBlog',
+    github: 'https://github.com/tiinsk/food-blog',
+    link: 'https://tiinas-food-blog.vercel.app',
+    designs: 'https://www.figma.com/file/LkCSHH9CmH4cpwzE9rOAUq/Food-blog?node-id=0%3A1',
+    images: [{
+      img: 'img_1.jpg',
+      thumb: 'thumb_1.jpg',
+    }, {
+      img: 'img_2.jpg',
+      thumb: 'thumb_2.jpg',
+    }, {
+      img: 'img_3.jpg',
+      thumb: 'thumb_3.jpg',
+    }, {
+      img: 'img_4.jpg',
+      thumb: 'thumb_4.jpg',
+    }],
+    topSkills: [
+      'react',
+      'nextjs',
+      'javascript',
+      'ui_design',
+      'figma',
+    ],
+    otherSkills: [
+      'node',
+      'css',
+      'styled_components',
+      'cms',
+      'contentful',
+      'vercel',
+    ]
+  },
+  {
     key: 'homeMonitor',
     github: 'https://github.com/tiinsk/sensor-frontend',
     link: 'https://home.tiina.dev',
@@ -29,35 +92,6 @@ export default [
       'ble',
       'noble',
       'raspberry',
-      'digital_ocean',
-      'nginx'
-    ]
-  },
-  {
-    key: 'thisIsMe',
-    github: 'https://github.com/tiinsk/ThisIsMe',
-    link: 'https://tiina.dev',
-    images: [{
-      img: 'img_1.jpg',
-      thumb: 'thumb_1.jpg',
-    }, {
-      img: 'img_2.jpg',
-      thumb: 'thumb_2.jpg',
-    }, {
-      img: 'img_3.jpg',
-      thumb: 'thumb_3.jpg',
-    }],
-    topSkills: [
-      'react',
-      'javascript',
-      'ui_design',
-      'sketch',
-    ],
-    otherSkills: [
-      'node',
-      'css',
-      'sass',
-      'git',
       'digital_ocean',
       'nginx'
     ]
