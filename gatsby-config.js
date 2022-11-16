50570167fc2b92e8ec549f6784ff7c2255426868
tiinsk <tiina.koskiranta@gmail.com>
2022-11-16 18:09:06 +0200

    remove unnecessary fonts and webfontloader (new gatsby gatsby-plugin-web-font-loader was already added)

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,9 +35,9 @@ module.exports = {
     options: {
       google: {
         families: [
-          'Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i',
-          'Lato:100,100i,300,300i,400,400i,700,700i,900,900i',
-          'Quicksand:300,400,500,700',
+          'Open+Sans:300,400,700',
+          'Lato:300,400,700',
+          'Quicksand:300,400,700',
           'PT+Serif:700',
           'Source+Code+Pro:400',
           'Material+Icons',
