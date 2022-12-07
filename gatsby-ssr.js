6f2d194742f7fd8ab8c65ef86dff9e294eccb896
tiinsk <tiina.koskiranta@gmail.com>
2022-12-07 21:28:38 +0200

    move global theme to layout.jsx to fix style loading issues

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {ThemeProvider} from 'styled-components';
+import { Provider } from 'react-redux';
 
 import configureStore from './src/configure-store';
-import {
-  GlobalStyle,
-  theme,
-} from './src/theme';
 
 const store = configureStore();
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Provider store={store}>
-        <div style={{height: '100%', overflow: 'hidden'}}>
-          {element}
-        </div>
-      </Provider>
-    </ThemeProvider>
-  )}
+    <Provider store={store}>
+      <div style={{ height: '100%', overflow: 'hidden' }}>{element}</div>
+    </Provider>
+  );
+};
