969f7b632d0b0a6bad99207095e84901135f818f
tiinsk <tiina.koskiranta@gmail.com>
2022-12-09 12:20:03 +0200

    fix unused imports

--- src/components/ui/presentational/header.jsx
+++ src/components/ui/presentational/header.jsx
@@ -1,5 +1,3 @@
-import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components/macro';
 import {GatsbyImage} from 'gatsby-plugin-image';
