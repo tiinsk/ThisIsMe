74693045d7eaa42296da0eecabddec905d5adc2b
tiinsk <tiina.koskiranta@gmail.com>
2022-12-10 15:28:27 +0200

    remove unused data and translation files

--- src/data/contacts.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: 'Tiina Koskiranta',
-  phone: '(+358) 050 348-9445',
-  email: 'tiina.koskiranta@gmail.com',
-  github: 'https://github.com/tiinsk',
-  linkedin: 'https://linkedin.com/in/tiinakoskiranta'
-}
